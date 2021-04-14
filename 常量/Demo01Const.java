74e0d656f73f9c0eb6e47286c7190d9373f76259
DESKTOP-MAAMLD7\DELL <13641315679@163.com>
2021-04-14 08:55:39 +0800

    常量

---
+++ 常量/Demo01Const.java
@@ -0,0 +1,30 @@
+/**
+常量：在程序运行期间，固定不变的量
+常量的分类：
+ 1、字符串常量：凡是用双引号引起来的部分，叫做字符串常量，例如："avc"，"helloworl"
+ 2、整数常量：直接写的数字，没有小数点,例如： 100，200
+ 3、浮点型常量：直接写上的数字，有小数点，例如：3.14，-0.34
+ 4、字符常量：凡是用单引号引起来的字符串，叫做字符常量，例如：'A'，'1'
+ 5、布尔常量：只有两个取值，true以及false
+ 6、空常量：null，代表没有任何数据
+ */
+ 
+ public class Demo01Const {
+   public static void main(String[] args) {
+    // 字符串常量
+    System.out.println("abs");
+ 
+    // 整数常量
+    System.out.println(100);
+ 
+    // 浮点型常量
+    System.out.println(3.14);
+ 
+    // 字符常量
+    System.out.println('A');
+ 
+    // 布尔值常量
+    System.out.println(true);
+    
+   }
+ }
