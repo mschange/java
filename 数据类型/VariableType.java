418e451d27c316e1eedf53a62b33da599b4fb760
DESKTOP-MAAMLD7\DELL <13641315679@163.com>
2021-04-14 09:19:40 +0800

    数据类型

---
+++ 数据类型/VariableType.java
@@ -0,0 +1,16 @@
+/**
+ * 变量：程序运行期间，内容可以发生改变的量
+ * 创建一个变量并使用的格式：
+ * 方式一： 数据类型 变量名称；// 创建变量
+ *          变量名称 = 数据值；
+ * 方式二： 数据类型 变量名称 = 数据值；
+ */
+public class VariableType {
+  public static void main(String[] args) {
+    int num1;
+    num1 = 1;
+    String str = "abcd";
+    System.out.println(num1);
+    System.out.println(str);
+  }
+}
